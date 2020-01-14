25cab111b0bf249dcbfa2b7ab70515f72ad2c41f
pingmyheart <pingmyheart@users.noreply.github.com>
2020-01-14 19:11:44 +0100

    Update Code.ino
    Fixed arduino code to works perfectly

--- Code.ino
+++ Code.ino
@@ -1,8 +1,7 @@
-#include <SoftwareSerial.h>
-#include<String.h>
+#include <SoftwareSerial.h>//including the library to permitt the comunication with the GSM Module
+#include<String.h>//including the library to manage the Strings
 
 SoftwareSerial GSMModule(9, 10);//In bracket indicate in order the Tx And Rx pin usend on Arduino Board
-
 
 void setup(){
     GSMModule.begin(9600);//Begin the comunication with GSM Module
@@ -10,25 +9,42 @@ void setup(){
     delay(1000);
 }
 
+int trace;//variable that contain the index of the backtick `
+String prefix;//variable for the prefix of the telephone number
+String telephone;//variable for the telephone number
+String text;//variable for the text that needs to be sent
+
 void loop(){
     String messages = Serial.readString();//Read the string passed through serial comunication
     
     //Incoming message looks like -----> prefix`cellphone_number`message_to_be_sent
-    String[] message = strtok(*messages,*'`');
-    String prefix = message[0];
-    String telephone = message[1];
-    String text = message[2];
+
+    /**
+     * The following code is used to split the string of the message in thre different parts:
+     *  -Prefix
+     *  -Telephone Number
+     *  -Message
+     * Using the Substring method of the class String
+    */
+    prefix = messages.substring(0,messages.indexOf('`'));
+    trace = messages.indexOf('`') + 1;
+    telephone = messages.substring(trace,messages.indexOf('`'));
+    trace = messages.indexOf('`') + 1;
+    text = messages.substring(trace,messages.length());
     SendMessage(prefix,telephone,text);
     delay(100);
 }
 
+/**
+ *The following is a function that is used to Send the message calling it into the main(loop) 
+*/
 void SendMessage(String a, String b, String c ){
   GSMModule.println("AT+CMGF=1");//Set the GSM Module to send texts
   delay(500);//Delay half second
-  GSMModule.println("AT+CMGS=\"+" + a + b + "\"\r");
+  GSMModule.println("AT+CMGS=\"+" + a + b + "\"\r");//Set the prefix and telephone number
   delay(500);//delay half second
-  GSMModule.println(c);//
+  GSMModule.println(c);//Set the message
   delay(100);//delay 100 milliseconds
-  GSMModule.println((char)26);//casting of the interruption command
+  GSMModule.println((char)26);//casting of the interruption command Ctrl+D
   delay(1000);
 }
