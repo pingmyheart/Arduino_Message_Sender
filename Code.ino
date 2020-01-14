9dd265e043cd4a40c7c9433fdd0689d2f49b50b8
pingmyheart <pingmyheart@users.noreply.github.com>
2020-01-14 18:40:02 +0100

    First Upgrade
    Fixed Errors in Arduino Code even if it is still unfinished

--- Code.ino
+++ Code.ino
@@ -11,8 +11,10 @@ void setup(){
 }
 
 void loop(){
-    String messages = Serial.ReadLine();
-    String[] message = messages.Split('`');
+    String messages = Serial.readString();//Read the string passed through serial comunication
+    
+    //Incoming message looks like -----> prefix`cellphone_number`message_to_be_sent
+    String[] message = strtok(*messages,*'`');
     String prefix = message[0];
     String telephone = message[1];
     String text = message[2];
@@ -21,12 +23,12 @@ void loop(){
 }
 
 void SendMessage(String a, String b, String c ){
-  mySerial.println("AT+CMGF=1");//Set the GSM Module to send texts
+  GSMModule.println("AT+CMGF=1");//Set the GSM Module to send texts
   delay(500);//Delay half second
-  mySerial.println("AT+CMGS=\""+"+"+a+b+"+\"\r");
+  GSMModule.println("AT+CMGS=\"+" + a + b + "\"\r");
   delay(500);//delay half second
-  mySerial.println(c);//
+  GSMModule.println(c);//
   delay(100);//delay 100 milliseconds
-   mySerial.println((char)26);//casting of the interruption 
+  GSMModule.println((char)26);//casting of the interruption command
   delay(1000);
 }
